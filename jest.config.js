64ea8c6097b43f4548878ba7d098d7fe624b95df
jiazhewang <neoddish@outlook.com>
2021-04-03 23:56:37 +0800

    init: project

---
+++ jest.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  testEnvironment: "node",
+  preset: "ts-jest",
+  globals: {
+    "ts-jest": {
+      diagnostics: false,
+    },
+  },
+  browser: false,
+  testPathIgnorePatterns: ["/node_modules/"],
+  collectCoverage: false,
+  testRegex: "/test/.*.test.ts?$",
+  verbose: false,
+};
---
+++ test/index.test.ts
@@ -0,0 +1,5 @@
+describe("test", () => {
+  it("hi", () => {
+    console.log("hello");
+  });
+});
