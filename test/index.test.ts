aa8a8def4ddcbcd40e07d0fa4bac2a1e0676d47e
jiazhewang <neoddish@outlook.com>
2021-04-04 11:25:32 +0800

    chore: init test cases

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,5 +1,52 @@
+import * as colorPaleteSchema from "../color-palette-schema.json";
+import { matchersWithOptions } from "jest-json-schema";
+expect.extend(
+  matchersWithOptions({
+    schemas: [colorPaleteSchema],
+  })
+);
+
+const colorAssets = {
+  brandName: "antd",
+  palettes: [
+    {
+      name: "trinity",
+      semantic: null,
+      type: "categorical",
+      colors: [
+        {
+          hex: "#ff0000",
+          rgb: [255, 0, 0],
+          name: "red",
+          nameCN: "红",
+          undertone: "warm",
+          usage: "danger",
+        },
+        {
+          hex: "#00ff00",
+          rgb: [0, 255, 0],
+          name: "green",
+          nameCN: "绿",
+          undertone: "neutral",
+        },
+        {
+          hex: "#0000ff",
+          rgb: [0, 0, 255],
+          name: "blue",
+          nameCN: "蓝",
+          undertone: "cool",
+        },
+      ],
+    },
+  ],
+};
+
 describe("test", () => {
+  test("schema is valid", () => {
+    expect(colorPaleteSchema).toBeValidSchema();
+  });
+
   it("hi", () => {
-    console.log("hello");
+    expect(colorAssets).toMatchSchema(colorPaleteSchema);
   });
 });
