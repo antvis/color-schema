db8951053b7ff3ed4825b8a311c2d1c18211117c
jiazhewang <neoddish@outlook.com>
2021-04-05 18:13:32 +0800

    feat: ts-json-schema generate

---
+++ src/schema/palette.ts
@@ -0,0 +1,28 @@
+import { Color } from "./color";
+
+export interface PaletteBaseInfo {
+  name: string;
+  semantic: string | null;
+}
+
+export type PaletteType = "categorical" | "discrete-scale" | "continuous-scale";
+
+export type CategoricalPalette = PaletteBaseInfo & {
+  type: "categorical";
+  colors: Color[];
+};
+
+export type DiscreteScalePalette = PaletteBaseInfo & {
+  type: "discrete-scale";
+  colors: Color[];
+};
+
+export type ContinuousScalePalette = PaletteBaseInfo & {
+  type: "continuous-scale";
+  colors: Color[];
+};
+
+export type Palette =
+  | CategoricalPalette
+  | DiscreteScalePalette
+  | ContinuousScalePalette;
