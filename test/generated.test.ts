ef2555f7c493e8b340ad6496a231597da444e8f6
neoddish <neoddish@outlook.com>
2021-04-09 23:58:41 +0800

    chore: demo (#2)
    * chore: antd example
    
    * temp init demo
    
    * chore: demo

--- test/generated.test.ts
+++ test/generated.test.ts
@@ -1,6 +1,7 @@
 import colorSchema from "../build/color-schema.json";
 import Ajv from "ajv";
 import { matchersWithOptions } from "jest-json-schema";
+import classic from "../examples/classic.json";
 
 expect.extend(
   matchersWithOptions({
@@ -8,71 +9,7 @@ expect.extend(
   })
 );
 
-const colorAssets = {
-  brandName: "antd",
-  palettes: [
-    {
-      name: "trinity",
-      semantic: null,
-      type: "categorical",
-      colors: [
-        {
-          space: "hex",
-          value: "#ff0000",
-          name: "red",
-          undertone: "warm",
-          usage: "danger",
-        },
-        {
-          space: "hex",
-          value: "#00ff00",
-          name: "green",
-          undertone: "neutral",
-        },
-        {
-          space: "hex",
-          value: "#0000ff",
-          name: "blue",
-          undertone: "cool",
-        },
-      ],
-    },
-    {
-      name: "scale1",
-      semantic: "热情奔放",
-      type: "discrete-scale",
-      colors: [
-        { space: "hex", value: "#fff7ec" },
-        { space: "hex", value: "#fdd49e" },
-        { space: "hex", value: "#fc8d59" },
-        { space: "hex", value: "#d7301f" },
-        { space: "hex", value: "#7f0000" },
-      ],
-    },
-    {
-      name: "scale1",
-      semantic: "静谧深邃",
-      type: "discrete-scale",
-      colors: [
-        { space: "hex", value: "#00f7ec" },
-        { space: "hex", value: "#00d49e" },
-        { space: "hex", value: "#008d59" },
-        { space: "hex", value: "#00301f" },
-        { space: "hex", value: "#000000" },
-      ],
-    },
-    {
-      name: "scale2",
-      semantic: "热情奔放",
-      type: "continuous-scale",
-      colors: [
-        { space: "hex", value: "#fff7ec" },
-        { space: "hex", value: "#fc8d59" },
-        { space: "hex", value: "#7f0000" },
-      ],
-    },
-  ],
-};
+const colorAssets = classic;
 
 describe("test", () => {
   test("schema itself is valid", () => {
