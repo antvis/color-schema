53046440a4c3df456f23ea60f3fafee1f2f1876e
jiazhewang <neoddish@outlook.com>
2021-05-25 00:06:22 +0800

    chore: webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,50 +1,58 @@
-const path = require('path');
-const webpack = require('webpack');
+const path = require("path");
+const webpack = require("webpack");
 
-module.exports = {
-  mode: 'development',
-  devtool: 'cheap-module-source-map',
+const prodConfig = {
+  mode: "production",
+  devtool: "cheap-module-source-map",
   entry: {
-    app: './demo/index',
+    app: "./src/index",
   },
   output: {
-    path: path.join(__dirname, './dist'),
-    filename: 'bundle.js',
-    chunkFilename: '[chunkhash].js',
-    publicPath: '/',
+    library: "ColorSchema",
+    libraryTarget: "umd",
+    path: path.resolve(__dirname, "dist/"),
+    filename: "index.js",
   },
-  externals: {
-    react: 'React',
-    'react-dom': 'ReactDOM',
-    antd: 'antd',
-  },
-  target: 'web',
   resolve: {
-    extensions: ['.js', '.ts', '.tsx', '.json'],
-    mainFields: ['module', 'browser', 'main'],
+    extensions: [".js", ".ts", ".tsx", ".json"],
+    mainFields: ["module", "browser", "main"],
   },
   module: {
     rules: [
       {
+        test: /\.ts/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+        options: {
+          presets: ["@babel/preset-typescript", ["@babel/preset-env", { modules: "commonjs" }]],
+          plugins: [
+            "@babel/plugin-transform-runtime",
+            "@babel/plugin-proposal-export-default-from",
+            "@babel/plugin-proposal-class-properties",
+            "@babel/plugin-proposal-object-rest-spread",
+          ],
+        },
+      },
+      {
         test: /\.(t|j)sx?$/,
         exclude: /node_modules/,
-        loader: 'babel-loader',
+        loader: "babel-loader",
         options: {
-          presets: ['@babel/preset-typescript', ['@babel/preset-env', { modules: 'commonjs' }], '@babel/preset-react'],
+          presets: ["@babel/preset-typescript", ["@babel/preset-env", { modules: "commonjs" }], "@babel/preset-react"],
           plugins: [
-            '@babel/plugin-transform-runtime',
-            '@babel/plugin-proposal-class-properties',
-            '@babel/plugin-proposal-object-rest-spread',
+            "@babel/plugin-transform-runtime",
+            "@babel/plugin-proposal-class-properties",
+            "@babel/plugin-proposal-object-rest-spread",
           ],
         },
       },
       {
         test: /\.(css|less)$/,
         use: [
-          { loader: 'style-loader' },
-          { loader: 'css-loader', options: { sourceMap: true } },
+          { loader: "style-loader" },
+          { loader: "css-loader", options: { sourceMap: true } },
           {
-            loader: 'less-loader',
+            loader: "less-loader",
             options: {
               sourceMap: true,
               lessOptions: {
@@ -58,10 +66,79 @@ module.exports = {
         test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
         use: [
           {
-            loader: 'file-loader',
+            loader: "file-loader",
             options: {
-              name: '[name].[ext]',
-              outputPath: 'fonts/',
+              name: "[name].[ext]",
+              outputPath: "fonts/",
+            },
+          },
+        ],
+      },
+    ],
+  },
+};
+
+const devConfig = {
+  mode: "development",
+  devtool: "cheap-module-source-map",
+  entry: {
+    app: "./demo/index",
+  },
+  output: {
+    path: path.join(__dirname, "./dist"),
+    filename: "bundle.js",
+    chunkFilename: "[chunkhash].js",
+    publicPath: "/",
+  },
+  externals: {
+    react: "React",
+    "react-dom": "ReactDOM",
+    antd: "antd",
+  },
+  target: "web",
+  resolve: {
+    extensions: [".js", ".ts", ".tsx", ".json"],
+    mainFields: ["module", "browser", "main"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(t|j)sx?$/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+        options: {
+          presets: ["@babel/preset-typescript", ["@babel/preset-env", { modules: "commonjs" }], "@babel/preset-react"],
+          plugins: [
+            "@babel/plugin-transform-runtime",
+            "@babel/plugin-proposal-class-properties",
+            "@babel/plugin-proposal-object-rest-spread",
+          ],
+        },
+      },
+      {
+        test: /\.(css|less)$/,
+        use: [
+          { loader: "style-loader" },
+          { loader: "css-loader", options: { sourceMap: true } },
+          {
+            loader: "less-loader",
+            options: {
+              sourceMap: true,
+              lessOptions: {
+                javascriptEnabled: true,
+              },
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "[name].[ext]",
+              outputPath: "fonts/",
             },
           },
         ],
@@ -70,14 +147,22 @@ module.exports = {
   },
   devServer: {
     disableHostCheck: true,
-    host: '0.0.0.0',
+    host: "0.0.0.0",
     hot: true,
-    contentBase: path.join(__dirname, 'demo'),
+    contentBase: path.join(__dirname, "demo"),
   },
   plugins: [
     new webpack.IgnorePlugin({ resourceRegExp: /^(fs|child_process)$/ }),
     new webpack.EnvironmentPlugin({
-      NODE_ENV: 'development',
+      NODE_ENV: "development",
     }),
   ],
+};
+
+module.exports = (env, argv) => {
+  if (argv.mode === "production") {
+    return prodConfig;
+  }
+
+  return devConfig;
 };
