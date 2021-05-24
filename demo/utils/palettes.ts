6afe87e44e0e4a1220b241f0134862e40c502dea
jiazhewang <neoddish@outlook.com>
2021-05-24 23:40:33 +0800

    fix: ts lint

--- demo/utils/palettes.ts
+++ demo/utils/palettes.ts
@@ -3,11 +3,11 @@ import { colorToHex } from "./colors";
 import { HexValue6, MatrixPalette } from "../../src";
 
 export function matrixToHex(matrix: MatrixPalette): HexValue6[][] {
-  const hexMatrix = [];
+  const hexMatrix: HexValue6[][] = [];
   const { x, y } = matrix.origin;
 
   y.forEach((color1) => {
-    const yColors = [];
+    const yColors: HexValue6[] = [];
 
     x.forEach((color2) => {
       const mixColor = chroma.mix(colorToHex(color1), colorToHex(color2)).hex();
